bd211bafb21fb4e402fb577d3dac8fc32a5ba921
MohamedKandadev <kandad.dev@gmail.com>
2023-11-16 22:30:27 +0000

    origin cros

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/', (req, res, next) => {
   res.status(200).json('Good')
 })
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
